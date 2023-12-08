bffde62dbeb0904a9676429cde8e7f656b2c09ff
forgottosave <forgottosave.ensel@tum.de>
2023-12-08 16:14:08 +0100

    implement replace-name conversion

--- olc.sh
+++ olc.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-echo "This script is not finished yet, please wait for the proper updates"
-
-calculate_relative_path() {
-    local source=$1
-    local target=$2
-
-    local common_part=$source
-    local result=""
-
-    while [[ "${target#$common_part}" == "${target}" ]]; do
-        common_part=$(dirname "$common_part")
-        result="../$result"
-    done
-
-    result="$result${target#$common_part/}"
-    echo "$result"
-}
-
-
-
-
-
 # check if given path is file or folder
 path="$PWD/$1"
 files=()
@@ -29,8 +7,6 @@ if [ -f "$path" ]; then
 	files+=("$path")
 elif [ -d "$path" ]; then
 	# find all files in directory
-	#files=($(find "$path" -type f | grep -vE "/(\.obsidian|\.git)/"))
-	# mapfile -t files << (find "$path" -type f -print0 | grep -zvE "/(\.obsidian|\.git)/")
 	while IFS= read -r -d '' file; do
     	files+=("$file")
 	done < <(find "$path" -type f -print0 | grep -zvE "/(\.obsidian|\.git)/")
@@ -47,23 +23,24 @@ for file in "${files[@]}"; do
 	while IFS= read -r line; do
 	    # Continue finding matches in the line until there are no more
 	    while [[ $line =~ $pattern ]]; do
-    	    linkmatch="${BASH_REMATCH[1]}"
+    	    match="${BASH_REMATCH[1]}"
 			# remove any obsidian sugar (like #chapter and |alternativename)
-			# TODO: make alternativename usable instead of just removing it
-			linkmatch="${linkmatch%'#'*}"
-        	echo "  Matched portion: $linkmatch"
+			linkreplacename="${match##*|}"
+			link="${match%'#'*}"
+        	echo "  Matched portion: $match, with replace-name: $linkreplacename"
 	        # Remove the found match from the line to find the next match
     	    line="${line#*${BASH_REMATCH[0]}}"
 			# find file that matches the link text
-			linkfile=$(find "$path" -type f -iname "$linkmatch.*")
+			linkfile=$(find "$path" -type f -iname "$link.*")
+			echo "    found file: $linkfile"
 			# convert path to relative path of file
-			relative_path=$(calculate_relative_path "$file" "$linkfile")
-			echo "    found according file: $relative_path"
+			relative_path=$(realpath "$linkfile" --relative-to="$file")
+			echo "    with relative path: $relative_path"
 			# replace markdown link
-			find="\[\[$linkmatch\]\]"
-			replace="\[$relative_path\]\($relative_path\)"
+			find="\[\[$match\]\]"
+			replace="\[$linkreplacename\]\($relative_path\)"
 			echo "    replacing \"$find\" with \"$replace\""
-			sed -i "s#$find#$replace#g" "$file"
+			sed -i "s#${find//#/\\#}#${replace//#/\\#}#g" "$file"
     	done
 	done < "$file"
 done
